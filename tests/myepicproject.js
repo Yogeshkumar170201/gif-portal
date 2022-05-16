ed90e6c0645b4fe2d64293dbe592c5771fc4a46b
Yogeshkumar170201 <55057594+Yogeshkumar170201@users.noreply.github.com>
2022-05-16 21:59:21 +0530

    First Solana Program

---
+++ tests/myepicproject.js
@@ -0,0 +1,48 @@
+const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
+
+const main = async() => {
+  console.log("🚀 Starting test...")
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Myepicproject;
+  const baseAccount = anchor.web3.Keypair.generate();
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+  console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+   await program.rpc.addGif("https://media.giphy.com/media/XckQuG5y1MqtSBHv16/giphy.gif", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+  
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  console.log('👀 GIF List', account.gifList)
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
